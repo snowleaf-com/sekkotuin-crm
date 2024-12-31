2e549599ec06e9f427200c54d467808ff3e492d1
snowleaf-com <yy.dec5@gmail.com>
2025-01-01 08:05:43 +0900

    vuetifyのtext-fieldを使うように変更

--- src/sekkotuin/resources/js/Components/TextInput.vue
+++ src/sekkotuin/resources/js/Components/TextInput.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
+const props = defineProps<{
+	label: string;
+}>();
+
 const model = defineModel<string>({ required: true });
 
 const input = ref<HTMLInputElement | null>(null);
@@ -15,9 +19,10 @@ defineExpose({ focus: () => input.value?.focus() });
 </script>
 
 <template>
-	<input
+	<v-text-field :label="label" ref="input" v-model="model" />
+	<!-- <input
 		ref="input"
 		v-model="model"
 		class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
-	/>
+	/> -->
 </template>
