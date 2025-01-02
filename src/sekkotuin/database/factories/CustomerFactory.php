4fbb1971c4cfc4e7291ac6baac7e4d6bcd0291cb
snowleaf-com <yy.dec5@gmail.com>
2025-01-03 07:39:46 +0900

    Customerダミーデータ作成

--- src/sekkotuin/database/factories/CustomerFactory.php
+++ src/sekkotuin/database/factories/CustomerFactory.php
@@ -16,8 +16,23 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
+      $tel = str_replace('-', '', fake()->phoneNumber());
+
         return [
-            //
+            'last_name' => fake()->lastName(),
+            'first_name' => fake()->firstName(),
+            'last_name_kana' => fake()->lastKanaName(),
+            'first_name_kana' => fake()->firstKanaName(),
+            'postcode' => fake()->postcode(),
+            'address' => implode(' ', [
+              fake()->prefecture(),
+              fake()->city(),
+              fake()->streetAddress(),
+            ]),
+            'tel' => $tel,
+            'birth' => fake()->dateTime(),
+            'gender' => fake()->numberBetween(0, 2),
+            'memo' => fake()->realText(50),
         ];
     }
 }
