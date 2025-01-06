95ab18e838f57c860d3cad51e7711a008fde0d02
snowleaf-com <yy.dec5@gmail.com>
2025-01-06 12:26:25 +0900

    一覧表示作成（v-tableにて）

--- src/sekkotuin/resources/js/Pages/Customers/Index.vue
+++ src/sekkotuin/resources/js/Pages/Customers/Index.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
   import { onMounted } from 'vue';
   import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-  import { Head } from '@inertiajs/vue3'; 
+  import { Head } from '@inertiajs/vue3';
+  // import { DataTableHeader } from '@/types/vuetify';
 
   const props = defineProps<{
     customers: {id: number, full_name: string, full_name_kana: string, created_at:string}[]
   }>()
+
+  // const headers:DataTableHeader[] = [
+  //   { title: 'ID', key: 'id' },
+  //   { title: '名前', key: 'full_name' },
+  //   { title: 'かな', key: 'full_name_kana' },
+  //   { title: '最終来院日', key: 'created_at' },
+  // ];
+
   onMounted(() => {
     console.log(props.customers);
   })
@@ -26,15 +35,36 @@
 		<div class="">
 			<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 				<div
-					class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
+					class="overflow-hidden bg-white shadow-lg sm:rounded-lg dark:bg-gray-800"
 				>
           <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-data-table
+            <v-row class="justify-center">
+              <v-col cols="10">
+                <!-- <v-data-table
                   :items="customers"
+                  :headers="headers"
+                  item-key="id"
                 >
-                </v-data-table>
+                </v-data-table> -->
+                <v-table density="compact">
+                  <thead>
+                    <tr>
+                      <th class="text-left">ID</th>
+                      <th class="text-right">名前</th>
+                      <th class="text-right">かな</th>
+                      <th class="text-right">最終来院日</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-for="item in customers" :key="item.id">
+                      <td>{{ item.id }}</td>
+                      <td class="text-right">{{ item.full_name }}</td>
+                      <td class="text-right">{{ item.full_name_kana }}</td>
+                      <td class="text-right">{{ item.created_at }}</td>
+                    </tr>
+                  </tbody>
+                </v-table>
               </v-col>
             </v-row>
           </v-container>
