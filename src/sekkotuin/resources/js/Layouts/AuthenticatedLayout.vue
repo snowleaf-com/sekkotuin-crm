45b1e7336541cacdbb3c59084f99573f48c09218
snowleaf-com <yy.dec5@gmail.com>
2025-01-29 12:35:07 +0900

    セクションタイトルのpadding調整

--- src/sekkotuin/resources/js/Layouts/AuthenticatedLayout.vue
+++ src/sekkotuin/resources/js/Layouts/AuthenticatedLayout.vue
@@ -82,7 +82,7 @@ const navigationLinks = [
 		<v-main>
 			<!-- ヘッダー -->
 			<header class="mt-4 bg-white" v-if="$slots.header">
-				<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+				<div class="mx-auto max-w-7xl md:px-6 md:py-6 xs:px-1 xs:y-2">
 					<slot name="header" />
 				</div>
 			</header>
